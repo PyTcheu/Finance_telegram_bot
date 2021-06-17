d31be6f0239b5a890a236979c56434463d5318c9
DaanJesus <danilo25oliveira@gmail.com>
2021-06-17 18:09:53 -0300

    alteração no comando /hey

--- travel_bot.py
+++ travel_bot.py
@@ -1,39 +1,32 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+import pandas as pd # Para evitar escrever pandas e trocar pela escrita apenas de pd para facilitar
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import yfinance as yf 
 import telegram
 import requests
 import telegram_send
-
 import schedule
 import time
-import os
 import random
+import os
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 from bs4 import BeautifulSoup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-
-# In[2]:
+from pandas_datareader import data as web # Evita a escrita do data e troca pelo web
+from datetime import datetime
 
 #Token PRD
 token = '1796099831:AAGteQJXlcNd2dLSSq0kbbqa0QoV0B_xtTU'
 
 #Token DEV
 #token = '1788977298:AAG7qKmlDhSazNpCMBjQ3GonWJzGLhj1Mno'
-
+    
 bot = telegram.Bot(token) #Replace TOKEN with your token string
 updater = Updater(token=token, use_context=True) #Replace TOKEN with your token string
 
 dispatcher = updater.dispatcher
-
-
-# In[3]:
 
 
 def get_coin_bid(coin):
@@ -42,8 +35,6 @@ def get_coin_bid(coin):
     c = coin.replace('-','')
     return result[c]['bid']
 
-
-# In[4]:
 
 
 def get_bdrs_price(paper):
@@ -59,9 +50,6 @@ def get_bdrs_price(paper):
         var = '+' + var
             
     return stock_price, var
-
-
-# In[5]:
 
 
 def get_fiis_price(paper):
@@ -81,11 +69,6 @@ def get_fiis_price(paper):
             var = '+' + var
             
         return stock_price, var
-    
-
-
-# In[6]:
-
 
 def get_stock_price(paper):
     response = 'https://statusinvest.com.br/acoes/' + paper
@@ -107,28 +90,15 @@ def get_stock_price(paper):
         return stock_price, var
     
     
-    
-    
-
-
-# In[7]:
-
-
 def stock(update, context):
     paper = " ".join(context.args)
     value = get_stock_price(paper.lower())
     update.message.reply_text(paper.upper() + ": R$" + value[0] + '\n \n Variação do dia: ' + value[1])
 
-
-# In[8]:
-
-
 def euro(update, context):
     eur = "Cotação do Euro Hoje: R$ " + str(get_coin_bid('EUR-BRL')).replace('.',',')
     context.bot.send_message(chat_id=update.effective_chat.id, text=eur)
 
-
-# In[9]:
 
 
 def vilzyn(update, context):
@@ -136,26 +106,17 @@ def vilzyn(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=dol)
 
 
-# In[10]:
-
-
 def dolar(update, context):
     dol = "Cotação do Dolar Hoje: R$ " + str(get_coin_bid('USD-BRL')).replace('.',',')
     context.bot.send_message(chat_id=update.effective_chat.id, text=dol)
 
 
-# In[11]:
-
-
-def hello(update, context): 
+def hey(update, context):
     photo = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRowHhdTB04EQSOcRuRTqLHvk96K4lS3UupYg&usqp=CAU', 'https://img.freepik.com/fotos-gratis/o-orangotango-jovem-sorriu-e-agiu-como_60359-323.jpg?size=626&ext=jpg', 'https://i.pinimg.com/originals/73/58/70/7358702b2780ce3933cf474c15035dde.jpg'] 
  
     caption = ['Eai Kamako', 'Aoba', 'Salve Kamakada'] 
      
     context.bot.sendPhoto(chat_id=update.effective_chat.id, photo = random.choice(photo), caption = random.choice(caption))
-
-
-# In[12]:
 
 
 def chart(update, context):
@@ -165,44 +126,11 @@ def chart(update, context):
     os.remove(paper + '.png')
 
 
-# In[ ]:
-
-
-
-
-
-# In[13]:
-
-
-import pandas as pd # Para evitar escrever pandas e trocar pela escrita apenas de pd para facilitar
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import yfinance as yf 
-
-from pandas_datareader import data as web # Evita a escrita do data e troca pelo web
-from datetime import datetime
-
-
-# def get_stock_chart(paper):
-#     paper_data = yf.download(
-#     tickers=paper + '.SA', 
-#     period="1d",
-#     interval="1m")
-#     
-#     plt.plot(paper_data['Close'])
-#     plt.savefig(paper + '.png')
-#     plt.clf()
-#     return paper_data
-
-# In[14]:
-
-
 def get_stock_chart(paper):
     data = yf.download(
     tickers=paper + '.SA', 
     period="3mo",
     interval="1d")
-    # Plot the close prices
 
     data = data.reset_index()
     data = data.rename(columns={data.columns[0]:'Date'})
@@ -216,11 +144,8 @@ def get_stock_chart(paper):
     fig.write_image(paper + '.png')
     
 
+dispatcher.add_handler(CommandHandler("hey", hey))
 
-# In[15]:
-
-
-dispatcher.add_handler(CommandHandler('hello', hello))
 dispatcher.add_handler(CommandHandler('dolar', dolar))
 dispatcher.add_handler(CommandHandler('euro', euro))
 dispatcher.add_handler(CommandHandler('vilzyn', vilzyn))
@@ -228,21 +153,4 @@ dispatcher.add_handler(CommandHandler('vilzyn', vilzyn))
 dispatcher.add_handler(CommandHandler("stock", stock))
 dispatcher.add_handler(CommandHandler("chart", chart))
 
-
-# In[16]:
-
-
 updater.start_polling()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
