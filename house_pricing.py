7688e443d2d2fcb4d6a9c34825c185d8b2cee4b8
unknown <pedro.tcheu@gmail.com>
2021-08-17 18:58:13 -0300

    Adding ML model

--- house_pricing.py
+++ house_pricing.py
@@ -1,4 +1,6 @@
 import pickle
+from sklearn.model_selection import GridSearchCV, cross_validate, cross_val_score, cross_val_predict
+from sklearn.ensemble import RandomForestRegressor
 
 filename = 'finalized_model.sav'
 
