70777d4b96cb2966d4d63ecef2f93a838167c2e1
unknown <pedro.tcheu@gmail.com>
2021-08-17 19:11:49 -0300

    Adding ML model

--- house_pricing.py
+++ house_pricing.py
@@ -6,8 +6,8 @@ filename = 'finalized_model.sav'
 
 hp_model = pickle.load(open(filename, 'rb'))
 
-def model_predict(cond, size, rooms, toilets, suits, parking, elevator, furnished, pool, new, district, neg_type, prop_type):
-    X = [cond, size, rooms, toilets, suits, parking, elevator, furnished, pool, new, district, neg_type, prop_type]
+def model_predict(param_list):
+    X = param_list
 
     predicted_price = hp_model.predict(X)
 
@@ -15,6 +15,6 @@ def model_predict(cond, size, rooms, toilets, suits, parking, elevator, furnishe
 
 
 def predict_house_price(update, context):
-    parameters = list(" ".join(context.args).split(' '))
+    parameters = " ".join(context.args).split(' ')
     final_price = model_predict(parameters)
     update.message.reply_text("O valor previsto para esse imovel é: " + str(final_price))
