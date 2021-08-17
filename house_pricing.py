242921cfda5cb6d6f4859de90dc6ca4499ad0c06
unknown <pedro.tcheu@gmail.com>
2021-08-17 19:44:26 -0300

    Adding ML model

--- house_pricing.py
+++ house_pricing.py
@@ -1,14 +1,22 @@
 import pickle
+import pandas as pd
+
 from sklearn.model_selection import GridSearchCV, cross_validate, cross_val_score, cross_val_predict
 from sklearn.ensemble import RandomForestRegressor
+
+cols = ['Condo', 'Size', 'Rooms', 'Toilets', 'Suites', 'Parking',
+       'Elevator', 'Furnished', 'Swimming Pool', 'New', 'District',
+       'Negotiation Type', 'Property Type']
+
 
 filename = 'finalized_model.sav'
 
 hp_model = pickle.load(open(filename, 'rb'))
 
 def model_predict(param_list):
-    X = param_list
+    X = pd.DataFrame(param_list, columns=cols)
 
+    X = X.reindex(labels = cols, axis = 1, fill_value = 0).drop(columns = ['Price'])
     predicted_price = hp_model.predict(X)
 
     return predicted_price
