153b83519eef624f2e0eecb0d2a21f2f1941b14b
unknown <pedro.tcheu@gmail.com>
2021-08-17 21:12:21 -0300

    Adding ML model

--- house_pricing.py
+++ house_pricing.py
@@ -7,9 +7,7 @@ from sklearn.ensemble import RandomForestRegressor
 df = pd.read_csv('sao-paulo-properties-april-2019.csv')
 df = df.iloc[:,:14]
 
-cols = ['Condo', 'Size', 'Rooms', 'Toilets', 'Suites', 'Parking',
-       'Elevator', 'Furnished', 'Swimming Pool', 'New', 'District',
-       'Negotiation Type', 'Property Type']
+cols = ['District', 'Negotiation Type', 'Property Type']
 
 
 filename = 'finalized_model.sav'
@@ -33,7 +31,10 @@ def predict_house_price(update, context):
 
 
 def get_dummies(data, df):
-    df_dummies = pd.get_dummies(data)
-    dummies_frame = pd.get_dummies(df.iloc[:,1:])
+    df_a = pd.DataFrame(data[:10])
+    df_dummies = pd.get_dummies(cols)
+    dummies_frame = pd.get_dummies(df.iloc[:,11:14])
     df_dummies = df_dummies.reindex(columns = dummies_frame.columns, fill_value=0)
-    return df_dummies
+    
+    df_final = pd.concat([df_a, df_dummies])
+    return df_final
